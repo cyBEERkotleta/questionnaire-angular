5d7fd6a493ecab15d30c4f40b693d7dc3cb80d95
Kate Levkovich <eeekotletka@gmail.com>
2024-03-07 22:31:07 +0300

    twentyth commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
-
 const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'dist/angular-questionnaire')));
+app.listen(process.env.PORT || 4200);
+
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-questionnaire/index.html'));
+  res.sendFile('index.html', {root: 'dist/angular-questionnaire/'});
 })
 
 console.log('Console listening!');
